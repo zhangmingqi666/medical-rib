24eed2fb74afd402f44a172f8c59875d16c81e9f
jiangyy5318 <jiangyy5318@gmail.com>
2019-03-05 09:58:21 +0800

    changes.log

---
+++ tools/test_header.py
@@ -0,0 +1,23 @@
+
+
+
+
+folder_path='../data/nii_files_merges/updated48labeled_1.31'
+import os, re
+import nibabel as nib
+pattern = re.compile('^[a-zA-Z1-9].*nii')
+for f in os.listdir(folder_path):
+    next_dir = os.path.join(folder_path, f)
+    if not os.path.isdir(next_dir):
+        continue
+
+    print("read folder {}".format(f))
+    for file_name in os.listdir(next_dir):
+
+        next_next_dir = os.path.join(next_dir, file_name)
+        if pattern.search(file_name) is None:
+            continue
+        img = nib.load(next_next_dir)
+        header = img.header
+        pixel_zoom = header.get_zooms()
+        print("read nii {}".format(file_name), pixel_zoom)
