26b4a9d7c156cb0ccd44fa138ff2eac2daa468c4
jiangyy5318 <jiangyy5318@gmail.com>
2019-03-05 11:24:22 +0800

    changes.log

--- experiments/scripts/merge_csv.sh
+++ experiments/scripts/merge_csv.sh
@@ -5,12 +5,17 @@ Prefix=$1
 output_path=$2
 rm -rf ${output_path}
 
-
-for f in ${Prefix}*
+files=$(ls ${Prefix})
+echo ${files}
+for f in ${files}
 do
+    echo ${f}
     if [[ ! -f "$output_path" ]]; then
+        echo "hhhhh"
+        touch ${output_path}
         cat ${f} > ${output_path}
     else
+        echo "lllll"
         cat ${f} | tail -n +2 >> ${output_path}
     fi
 done
