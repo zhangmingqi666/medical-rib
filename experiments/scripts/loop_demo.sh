7dd83df96cc0396ff5ace364dee923b7848d2bee
jiangyouyou <youyoujiang@matrix.io>
2019-10-16 16:24:38 +0800

    changes.log

--- experiments/scripts/loop_demo.sh
+++ experiments/scripts/loop_demo.sh
@@ -2,8 +2,8 @@
 
 ct_data_dir=/home/jiangyy/projects/medical-rib/data/dicom_files_merges/RIB_Batch1
 demo_dir=/home/jiangyy/projects/medical-rib/data/demo_dir
-files=$(ls ${ct_data_dir})
-
+#files=$(ls ${ct_data_dir})
+files=("135402000697720" "135402000698890" "135402000699104" "135402000699174" "135402000699173")
 for f in ${files}
 do
     echo "test ${f}"
